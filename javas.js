e1e993498120c85b6e6f3bc04b8dc2729f27da7f
KRINBoven <146179484+KRINBoven@users.noreply.github.com>
2023-09-27 16:21:17 +0700

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,37 @@
+const bar= document.getElementById('bar');
+const nav= document.getElementById('navbar');
+const close= document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click' , ()=>{
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active');
+    })
+}
+
+/*------------Single product ------------*/
+
+var mainImg= document.getElementById('mainImg');
+var smallImg= document.getElementsByClassName('smallImg');
+
+smallImg[0].onclick = function(){
+    mainImg.src= smallImg[0].src
+}
+smallImg[1].onclick = function(){
+    mainImg.src= smallImg[1].src
+}
+smallImg[2].onclick = function(){
+    mainImg.src= smallImg[2].src
+}
+smallImg[3].onclick = function(){
+    mainImg.src= smallImg[3].src
+}
+
+
+
+
